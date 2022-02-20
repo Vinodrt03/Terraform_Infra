facaeabe08d9aded24a3e6ed5a3e634714c4e287
vinodrt03 <vinod.rawat0311@gmail.com>
2022-02-20 15:17:12 +0530

    java

--- ec2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  profile    = "default"
-  region     = "ap-south-1"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0851b76e8b1bce90b"
-  instance_type = "t2.micro"
-}
