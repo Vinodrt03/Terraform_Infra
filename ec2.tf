590f32960fceaff798b5c432ef9a46e5a26b5713
vinodrt03 <vinod.rawat0311@gmail.com>
2022-02-20 22:53:24 +0530

    fixed

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-resource "aws_instance" "Web-server" {
+resource "aws_instance" "Webserver" {
   ami           = "0851b76e8b1bce90b "
   instance_type = "t2.micro"
 }
